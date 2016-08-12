e47a7d02d43f7a3b510d7e2e763c1166473e1600
CabbageOni <pos02232@gmail.com>
2016-08-12 14:12:04 +0900

    added missing files

---
+++ Prototype/Assets/Scripts/Customer/CustomerLogic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class CustomerLogic : MonoBehaviour
+{
+    public GameObject OrderingBallon = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Resources/Prefab/OrderingBallon.prefab");
+
+    //order menu instantly.
+	void Start ()
+    {
+        Instantiate(OrderingBallon, transform.position + new Vector3(0,transform.localScale.y * 2,0), Quaternion.identity);
+	}
+}
---
+++ Prototype/Assets/Scripts/OrderingBallonLogic.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OrderingBallonLogic : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(3);
+    } 
+}
