d069ba233efc57f3bbacad13ff6fa57a51e52b68
CabbageOni <pos02232@gmail.com>
2016-08-15 13:42:56 +0900

    fixed bug

--- Prototype/Assets/Scripts/Customer/CustomerLogic.cs
+++ Prototype/Assets/Scripts/Customer/CustomerLogic.cs
@@ -4,11 +4,15 @@ using System.Collections;
 
 public class CustomerLogic : MonoBehaviour
 {
-    public GameObject OrderingBallon = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Resources/Prefab/OrderingBallon.prefab");
+    public GameObject OrderingBallon;
 
     //order menu instantly.
 	void Start ()
     {
+        //the default ballon
+        if(OrderingBallon == null)
+            OrderingBallon = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Resources/Prefab/OrderingBallon.prefab");
+
         Instantiate(OrderingBallon, transform.position + new Vector3(0,transform.localScale.y * 2,0), Quaternion.identity);
 	}
 }
