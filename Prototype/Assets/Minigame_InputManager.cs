78734ad7fd934983838a52743333ae9cc9c8823c
CabbageOni <pos02232@gmail.com>
2016-08-27 20:39:26 -0700

    fixed some bugs

---
+++ Prototype/Assets/Minigame_InputManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Minigame_InputManager : MonoBehaviour
+{	
+	void Update ()
+    {
+	    if(Input.GetKeyDown("escape"))
+        {
+            SceneManager.LoadScene(2);
+        }
+	}
+}
