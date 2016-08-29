411c0c19599325c9142fe350b0c664829cf3c698
CabbageOni <pos02232@gmail.com>
2016-08-29 00:03:39 -0700

    working on saving cuplists

---
+++ Prototype/Assets/FinishedOrderList.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class FinishedOrderList : MonoBehaviour
+{
+    void Awake ()
+    {
+        //if duplicate, erase this
+        if (GameObject.Find("[[Finished Orders]]") != null)
+            Destroy(gameObject);
+    }
+
+    void Start ()
+    {
+        //change name to specify this object
+        transform.name = "[[Finished Orders]]";
+
+        //make this saved between scenes
+        DontDestroyOnLoad(this);
+    }
+
+    public void OnLevelWasLoaded(int level)
+    {
+        //if main level, create lists
+        if (level == Scenes.MainLevel)
+        {
+            GameObject cup = GameObject.Instantiate(Resources.Load<GameObject>("Prefab/cup"));
+            cup.transform.SetParent(GameObject.Find("UI").transform, false);
+
+            Debug.Log("created!");
+        }
+    }
+}
--- Prototype/Assets/Scripts/Customer/OrderLogic.cs
+++ Prototype/Assets/Scripts/Customer/OrderLogic.cs
@@ -21,39 +21,6 @@ public class OrderLogic : MonoBehaviour
         OriginalPosition = rt.position;
     }
 
-    //void Start ()
-    //{
-    //    //saved between multi-scene
-    //    DontDestroyOnLoad(this);
-    //}
-
-    //void OnLevelWasLoaded(int index)
-    //{
-    //    //make it invisible when not in neither minigame nor mainlevel
-    //    if (index != Scenes.MainLevel && index != Scenes.Minigame)
-    //    {
-    //        gameObject.SetActive(false);
-    //    }
-    //    else
-    //    {
-    //        gameObject.SetActive(true);
-
-    //        //make it follow camera (ortho)
-    //        //if(index == Scenes.MainLevel)
-    //        //{
-    //        //    GameObject Camera = GameObject.Find("Main Camera");
-
-    //        //    if(Camera != null)
-    //        //    {
-    //        //        //transform.position = Camera.transform.position;
-    //        //        //transform.position += Camera.transform.forward;
-    //        //        //transform.parent = Camera.transform;
-    //        //        transform.parent = GameObject.Find("UI").transform;
-    //        //    }
-    //        //}
-    //    }
-    //}
-
     public void DragSelectedCup()
     {
         dragging = true;
