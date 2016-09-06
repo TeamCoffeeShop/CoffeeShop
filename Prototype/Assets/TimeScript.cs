42529c27d4a529f4fd425676460f1ea62a4c98d3
saerom <sslom90@gmail.com>
2016-09-06 14:14:14 -0700

    Added TimeScript.cs

---
+++ Prototype/Assets/TimeScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeScript : MonoBehaviour {
+
+    public GameObject time;
+    GameObject timetxtobj;
+
+    // Use this for initialization
+    void Start()
+    {
+        time = GameObject.Find("TimeOfDay");
+        timetxtobj = GameObject.Find("time");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int currentHour = (int)time.GetComponent<TimeOfDay>().currentHour;
+        int currentMin = (int)time.GetComponent<TimeOfDay>().currentMin;
+        timetxtobj.GetComponent<Text>().text = currentHour + ":" + currentMin;
+    }
+}
