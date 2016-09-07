ac97249f70625f0ed3324fd681d282adc83d5789
CabbageOni <pos02232@gmail.com>
2016-09-07 00:29:52 -0700

    adding money

--- Prototype/Assets/Scripts/UI/MoneyUIScript.cs
+++ Prototype/Assets/Scripts/UI/MoneyUIScript.cs
@@ -1,15 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoneyUIScript : MonoBehaviour {
+public class MoneyUIScript : MonoBehaviour
+{
+    public GameObject MoneyText;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Update ()
+    {
+
+    }
 }
