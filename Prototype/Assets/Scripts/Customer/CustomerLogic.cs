861d989297381ca66d9da0b20603ce83b3576944
saerom <sslom90@gmail.com>
2016-09-07 00:21:56 -0700

    Added customer spawn timer

--- Prototype/Assets/Scripts/Customer/CustomerLogic.cs
+++ Prototype/Assets/Scripts/Customer/CustomerLogic.cs
@@ -5,7 +5,11 @@ using System.Collections;
 public class CustomerLogic : MonoBehaviour
 {
     public GameObject OrderingBallon;
+    public GameObject SpawnTimer;
 
+    private GameObject ST;
+
+    float time = 0.0f;
     //order menu instantly.
 	void Start ()
     {
@@ -13,11 +17,26 @@ public class CustomerLogic : MonoBehaviour
         if(OrderingBallon == null)
             OrderingBallon = Resources.Load<GameObject>("Prefab/OrderingBallon");
 
+        //the default spawnTimer
+        if (SpawnTimer == null)
+            SpawnTimer = Resources.Load<GameObject>("Prefab/SpawnBar");
+
         GameObject OB = Instantiate(OrderingBallon);
         OB.transform.SetParent(GameObject.Find("UI").transform, false);
         OB.GetComponent<OrderingBallonLogic>().customer = transform;
 
+        ST = Instantiate(SpawnTimer);
+        ST.transform.SetParent(GameObject.Find("UI").transform, false);
+        ST.GetComponent<CustomerSpawnTimer>().customer = transform;
+        
         //custom cup display
         CoffeeOrderSetup.SetOrder(ref OB, GetComponent<Customer>().data.order);
-	}
+    }
+
+    void Update()
+    {
+        time += 0.005f;
+        ST.GetComponent<BarScript>().Value = time;
+        ST.GetComponent<BarScript>().MaxValue = 1.0f;
+    }
 }
