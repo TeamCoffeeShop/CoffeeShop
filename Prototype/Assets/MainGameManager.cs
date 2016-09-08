9e44e801cfce6bb9ddc2a8f220317719389c9f82
saerom <sslom90@gmail.com>
2016-09-08 14:58:41 -0700

    Added MainGameManager.cs file

---
+++ Prototype/Assets/MainGameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainGameManager : MonoBehaviour {
+
+    public bool OnDialogue;
+    public NPCDialogue dialoguemanager;
+
+	// Use this for initialization
+	void Start () {
+
+        OnDialogue = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (OnDialogue)
+        {
+            dialoguemanager.GetComponentInChildren<Canvas>().enabled = true;
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            dialoguemanager.GetComponentInChildren<Canvas>().enabled = false;
+            Time.timeScale = 1.0f;
+        }
+	}
+}
