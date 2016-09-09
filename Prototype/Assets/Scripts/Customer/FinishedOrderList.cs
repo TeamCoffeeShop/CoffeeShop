38fe482c795b2f054f2ceb85c52b4149eb5e17ac
CabbageOni <pos02232@gmail.com>
2016-09-08 21:46:43 -0700

    fixd prefabs & dialogue.

--- Prototype/Assets/Scripts/Customer/FinishedOrderList.cs
+++ Prototype/Assets/Scripts/Customer/FinishedOrderList.cs
@@ -50,34 +50,37 @@ public class FinishedOrderList : MonoBehaviour
 
     void CreateOrdersInUI()
     {
-        //create cups as much as childrens
-        int size = orders.childCount;
-        float xPos = 0;
-        for (int i = 0; i < size; ++i)
+        if (orders)
         {
-            //create cup
-            GameObject cup = GameObject.Instantiate(orderDisplayButton);
-            cup.transform.SetParent(OrderIcon, false);
+            //create cups as much as childrens
+            int size = orders.childCount;
+            float xPos = 0;
+            for (int i = 0; i < size; ++i)
+            {
+                //create cup
+                GameObject cup = GameObject.Instantiate(orderDisplayButton);
+                cup.transform.SetParent(OrderIcon, false);
 
-            //set cup details here
-            CoffeeCupBehavior finishedCup = orders.GetChild(i).GetComponent<CoffeeCupBehavior>();
-            OrderLogic currentCup = cup.GetComponent<OrderLogic>();
-            currentCup.originalCup = finishedCup;
-            currentCup.OrderManager = this;
-            currentCup.ChildNumber = i;
+                //set cup details here
+                CoffeeCupBehavior finishedCup = orders.GetChild(i).GetComponent<CoffeeCupBehavior>();
+                OrderLogic currentCup = cup.GetComponent<OrderLogic>();
+                currentCup.originalCup = finishedCup;
+                currentCup.OrderManager = this;
+                currentCup.ChildNumber = i;
 
-            //distinguish image by the droptype
-            CoffeeOrderSetup.SetOrder(ref cup, finishedCup.DistinguishedMenuName);
+                //distinguish image by the droptype
+                CoffeeOrderSetup.SetOrder(ref cup, finishedCup.DistinguishedMenuName);
 
-            //after distinguishing, deactivate original cup
-            finishedCup.gameObject.SetActive(false);  
+                //after distinguishing, deactivate original cup
+                finishedCup.gameObject.SetActive(false);
 
-            //set Transform (to not stack in one place)
-            RectTransform rt = cup.GetComponent<RectTransform>();
-            float gap = CalculateGap(rt);
-            
-            rt.Translate(xPos, 0, 0);
-            xPos += gap;
+                //set Transform (to not stack in one place)
+                RectTransform rt = cup.GetComponent<RectTransform>();
+                float gap = CalculateGap(rt);
+
+                rt.Translate(xPos, 0, 0);
+                xPos += gap;
+            }
         }
     }
 
