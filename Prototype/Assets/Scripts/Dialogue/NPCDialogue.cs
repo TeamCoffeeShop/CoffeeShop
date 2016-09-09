0aff2d9abf8a0415553561db9fc7f4f54b3e7888
CabbageOni <pos02232@gmail.com>
2016-09-08 17:30:51 -0700

    added dialogue

--- Prototype/Assets/Scripts/Dialogue/NPCDialogue.cs
+++ Prototype/Assets/Scripts/Dialogue/NPCDialogue.cs
@@ -8,47 +8,56 @@ using UnityEngine.SceneManagement;
 public class NPCDialogue : MonoBehaviour
     {
         private Dialogue dia;
-    private GameObject dialogue_window; //Dialogue Panel
-    private GameObject npc_text; //NPC Dialogue
-    private GameObject character_text; //Character Dialogue
-    private GameObject option1; //Option 1
-    private GameObject option2; //Option 2
+        private GameObject dialogue_window; //Dialogue Panel
+        private GameObject npc_text; //NPC Dialogue
+        private GameObject character_text; //Character Dialogue
+        private GameObject option1; //Option 1
+        private GameObject option2; //Option 2
 
-    private GameObject next;
-                                   //////////////////////////////////
-
-    public string option1Text;
-    public string option2Text;
-    public int optionCount;
+        private GameObject gamemanager;
 
         private int selected_option = -2;
 
-        public string DialogueDataFilePath; //Xml file
+        public string Dialogue1Path; //Xml file
+        public string Dialogue2Path;
+
         public GameObject DialogueWindowPrefab; //Dialogue Panel Prefab
 
-    public int gameLevel;
+        public int gameLevel;
 
         //Use this for initialization
         void Start()
         {
+        gamemanager = GameObject.Find("MainGameManager");
 
-            dia = load_dialogue("Assets/Resources/Xmls/" + DialogueDataFilePath);
-            var canvas = GameObject.Find("Canvas");
+        var canvas = GameObject.Find("Canvas");
 
-            dialogue_window = Instantiate<GameObject>(DialogueWindowPrefab);
-            dialogue_window.transform.SetParent(canvas.transform, false);
+        dialogue_window = Instantiate<GameObject>(DialogueWindowPrefab);
+        dialogue_window.transform.SetParent(canvas.transform, false);
 
-            RectTransform dia_window_transform = (RectTransform)dialogue_window.transform;
-            dia_window_transform.localPosition = new Vector3(0, -50, 0);
+        RectTransform dia_window_transform = (RectTransform)dialogue_window.transform;
+        dia_window_transform.localPosition = new Vector3(0, -50, 0);
 
         npc_text = GameObject.Find("NPCDialogue_Text");
         character_text = GameObject.Find("CharacterDialogue_Text");
-            option1 = GameObject.Find("Option1");
-            option2 = GameObject.Find("Option2");
+        option1 = GameObject.Find("Option1");
+        option2 = GameObject.Find("Option2");
 
-            dialogue_window.SetActive(false);
+        dialogue_window.SetActive(false);
+
+        if (PlayerPrefs.GetInt("DialogueID") == 0)
+        {
+            dia = load_dialogue("Assets/Resources/Xmls/" + Dialogue1Path);
 
             RunDialogue();
+        }
+        else if(PlayerPrefs.GetInt("DialogueID") == 1)
+        {
+            dia = load_dialogue("Assets/Resources/Xmls/" + Dialogue2Path);
+
+            RunDialogue();
+        }
+
         }
 
         public void RunDialogue()
@@ -68,7 +77,6 @@ public class NPCDialogue : MonoBehaviour
             //create an indexer, set it to 0 - the start node
             int node_id = 0;
 
-
             //while the next node is not an exit node, traverse the dialogue tree based on user input
             while (node_id != -1)
             {
@@ -76,13 +84,19 @@ public class NPCDialogue : MonoBehaviour
                 selected_option = -2;
                 while (selected_option == -2)
                 {
-                    yield return new WaitForSeconds(0.1f);
+                //gamemanager.GetComponent<MainGameManager>().OnDialogue = false;
+				yield return null;
                 }
                 node_id = selected_option;
-                
             }
             dialogue_window.SetActive(false);
-        SceneManager.LoadScene(gameLevel);
+        gamemanager.GetComponent<MainGameManager>().OnDialogue = false;
+        if (PlayerPrefs.GetInt("DialogueID") == 0)
+        {
+            PlayerPrefs.SetInt("DialogueID", 1);
+            Time.timeScale = 1.0f;
+            SceneManager.LoadScene(gameLevel);
+        }
     }
 
         private void display_node(DialogueNode node)
@@ -92,18 +106,15 @@ public class NPCDialogue : MonoBehaviour
             option1.SetActive(false);
             option2.SetActive(false);
 
-        optionCount = node.options.Count;
             for (int i = 0; i < node.options.Count && i < 2; ++i)
             {
                 switch (i)
                 {
                     case 0:
                         set_option_button(option1, node.options[i]);
-                    option1Text = node.options[i].tempText;
                         break;
                     case 1:
                         set_option_button(option2, node.options[i]);
-                    option2Text = node.options[i].tempText;
                         break;
                 }
             }
--- Prototype/Assets/Scripts/Scene/MainMenuScene.cs
+++ Prototype/Assets/Scripts/Scene/MainMenuScene.cs
@@ -6,6 +6,7 @@ public class MainMenuScene : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         PlayerPrefs.DeleteAll();
+        PlayerPrefs.SetInt("DialogueID", 0);
 	}
 	
 	// Update is called once per frame
