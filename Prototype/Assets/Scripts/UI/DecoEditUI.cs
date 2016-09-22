d792cd76cec76b670402e5222f5c7547ed662e05
CabbageOni <pos02232@gmail.com>
2016-09-22 02:20:19 -0700

    working on moving obj..

--- Prototype/Assets/Scripts/UI/DecoEditUI.cs
+++ Prototype/Assets/Scripts/UI/DecoEditUI.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class DecoEditUI : MonoBehaviour
 {
     public Grid EditingGrid;
+    bool Dragging = false;
+    Vector3 PrevMousePosition;
+    GameObject DecoCopy;
+
+    float CameraAngle;
+    float MoveSpeed = 0.1f;
 
     public void Selected (Grid grid)
     {
@@ -36,5 +42,45 @@ public class DecoEditUI : MonoBehaviour
         Grid tempGrid = EditingGrid;
         DisableSelected();
         DestroyObject(tempGrid.transform.GetChild(0).gameObject);
+    }
+
+    public void MoveBegin ()
+    {
+        Dragging = true;
+        Cursor.visible = false;
+        PrevMousePosition = Input.mousePosition;
+
+        //temporary. will change soon.
+        DecoCopy = Instantiate(EditingGrid.transform.GetChild(0).gameObject);
+        DecoCopy.transform.localScale = EditingGrid.transform.GetChild(0).lossyScale;
+        DecoCopy.transform.position = EditingGrid.transform.GetChild(0).position;
+    }
+
+    public void MoveEnd ()
+    {
+        Dragging = false;
+        Cursor.visible = true;
+        DestroyObject(DecoCopy);
+    }
+
+    void Start ()
+    {
+        CameraAngle = -Quaternion.ToEulerAngles(MainGameManager.Get.maincamera.transform.rotation).y;
+    }
+
+    void Update ()
+    {
+        if (Dragging)
+        {
+            Vector3 Move = Input.mousePosition - PrevMousePosition;
+            PrevMousePosition = Input.mousePosition;
+
+            Vector2 newMove;
+            newMove.x = Mathf.Cos(CameraAngle) * Move.x + -Mathf.Sin(CameraAngle) * Move.y;
+            newMove.y = Mathf.Sin(CameraAngle) * Move.x + Mathf.Cos(CameraAngle) * Move.y;
+
+            DecoCopy.transform.position += new Vector3(newMove.x * MoveSpeed, 0, newMove.y * MoveSpeed);
+
+        }
     }
 }
