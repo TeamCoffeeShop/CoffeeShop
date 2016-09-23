1e57b729a84fcdce357a771e279c06d8cc309459
saerom <sslom90@gmail.com>
2016-09-23 13:38:18 -0700

    Fixed money, xp system Added Score/Result screen at the end of the day

--- Prototype/Assets/Scripts/Scene/CalculateScene.cs
+++ Prototype/Assets/Scripts/Scene/CalculateScene.cs
@@ -6,16 +6,29 @@ public class CalculateScene : MonoBehaviour
 {
     private GameObject gameobject;
 
-    public GameObject incomeText;
+    public GameObject CurrentMoney;
+    public GameObject MaintenanceExpense;
+    public GameObject Total;
 	// Use this for initialization
 	void Start () {
+        MainGameManager.Get.TimeOfDay.enabled = false;
+        float current_money = MainGameManager.Get.playerManager.player.money;
+        float maintenance_expense = 200;
+        float total = current_money - maintenance_expense;
+        MainGameManager.Get.playerManager.SubtractMoneyGradully(maintenance_expense);
 
         gameobject = GameObject.Find("CustomerList(Clone)");
         Destroy(gameobject);
-        Destroy(MainGameManager.Get.TimeOfDay.gameObject);
 
-        incomeText = GameObject.Find("income");
-        incomeText.GetComponent<Text>().text = "Income : " + PlayerPrefs.GetFloat("income").ToString();
+
+        CurrentMoney = GameObject.Find("CurrentMoney");
+        CurrentMoney.GetComponent<Text>().text = "Current Money : " + current_money;
+
+        MaintenanceExpense = GameObject.Find("MaintenanceExpense");
+        MaintenanceExpense.GetComponent<Text>().text = "Maintenance Expense : " + maintenance_expense;
+
+        Total = GameObject.Find("Total");
+        Total.GetComponent<Text>().text = "Total : " + total;
     }
 	
 	// Update is called once per frame
