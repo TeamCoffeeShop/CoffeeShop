55404f025785c8b66fc2de861dd2d05de73ccaed
saerom <sslom90@gmail.com>
2016-10-05 13:45:05 -0700

    Added pull handle mechanic for selecting hot and ice( it doesn't finish yet)

---
+++ Prototype/Assets/Scripts/CoffeeBean/ObjectClickScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectClickScript : MonoBehaviour {
+
+    public bool CheckRotation = false;
+
+    void OnMouseDown()
+    {
+        CheckRotation = true;
+    }
+
+    void OnMouseUp()
+    {
+        CheckRotation = false;
+    }
+}
