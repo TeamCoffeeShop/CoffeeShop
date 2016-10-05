c93948656a76f792b10ce13478a607edf7fc9a49
CabbageOni <pos02232@gmail.com>
2016-10-05 14:11:12 -0700

    adding liquid falling logic

---
+++ Prototype/Assets/Scripts/~Etc/LiquidFalling/CustomFallingEffect.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomFallingEffect : MonoBehaviour
+{
+    public enum DropType
+    {
+        liquid
+    }
+
+    public DropType[] type;
+    public Material[] material;
+    public Vector3 beginPosition;
+    public Vector3 endPosition;
+    
+    bool fall;
+
+    GameObject liquidPrefab;
+    GameObject currLiquid;
+
+    void Start ()
+    {
+        liquidPrefab = Resources.Load<>();
+    }
+
+    public void StartFalling ()
+    {
+        if(!fall)
+        {
+            fall = true;
+        }
+    }
+
+    public void EndFalling ()
+    {
+        if (fall)
+        {
+            fall = true;
+        }
+    }
+
+    public void ToggleFalling ()
+    {
+        if (fall) EndFalling();
+        else StartFalling();
+    }
+}
---
+++ Prototype/Assets/Scripts/~Etc/LiquidFalling/FallingLiquid.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingLiquid : MonoBehaviour
+{
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+	
+	}
+}
