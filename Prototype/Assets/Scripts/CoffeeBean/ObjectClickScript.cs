a124a1d5095462713d5081ddbcda2f48d11cd0a6
saerom <sslom90@gmail.com>
2016-10-06 16:22:58 -0700

    Delayed the time for cup creation & working on hot and ice selection

--- Prototype/Assets/Scripts/CoffeeBean/ObjectClickScript.cs
+++ Prototype/Assets/Scripts/CoffeeBean/ObjectClickScript.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class ObjectClickScript : MonoBehaviour {
 
+    public GameObject pullHandle;
+    public Transform pivotTransform;
     public bool MouseChakable = true;
     public bool CheckRotation = false;
 
     public Vector3 prevMousePos;
+    public Vector3 curMousePos;
+
+    public bool check = false;
+
+    private bool buttonSelected = false;
     void OnMouseDown()
     {
         CheckRotation = true;
@@ -15,28 +22,43 @@ public class ObjectClickScript : MonoBehaviour {
 
     void OnMouseDrag()
     {
-        if (MouseChakable)
+        curMousePos = Input.mousePosition;
+        if (MouseChakable && buttonSelected == false)
         {
-            if (Input.mousePosition.x - prevMousePos.x <2)
-            {
-                MouseChakable = false;
-            }
+            Vector3 MousePos = Input.mousePosition;
+            Vector3 MousePosDelta = MousePos - prevMousePos;
 
-            else if (Input.mousePosition.x - prevMousePos.x < -2)
+            //Left
+            if (Input.mousePosition.x - prevMousePos.x <150 && MousePosDelta.x > 0)
             {
                 MouseChakable = false;
+                pullHandle.transform.RotateAround(pivotTransform.position, new Vector3(0, 1, 0), -1* 40);
+                buttonSelected = true;
             }
+            //Right
+            else if (Input.mousePosition.x - prevMousePos.x > -150 && MousePosDelta.x < 0)
+            {
+                MouseChakable = false;
+                pullHandle.transform.RotateAround(pivotTransform.position, new Vector3(0, 1, 0), 1* 40);
+                buttonSelected = true;
+            }
+        }
+
+        if (buttonSelected)
+        {
+
         }
     }
     void OnMouseUp()
     {
         CheckRotation = false;
-        //MouseChakable = true;
+        MouseChakable = true;
     }
 
     void OnCollisionEnter()
     {
         if(gameObject.tag == "CoffeeCondition")
             CheckRotation = false;
+
     }
 }
--- Prototype/Assets/Scripts/Make Order/CoffeeCupSelector.cs
+++ Prototype/Assets/Scripts/Make Order/CoffeeCupSelector.cs
@@ -4,20 +4,43 @@ using System.Collections;
 public class CoffeeCupSelector : MonoBehaviour
 {
     public GameObject CoffeeCupPrefab;
+    public bool active;
+    private float time = 0;
+    private Vector3 pos;
 
     void Start ()
     {
+        active = true;
         GetComponent<DragandDrop>().Target[0] = MinigameManager.Get.coffeeMachine.gameObject;
     }
 
     void OnMouseDown ()
     {
-        //create new cup
-        GetComponent<Collider>().isTrigger = false;
-        GetComponent<Rigidbody>().isKinematic = false;
-        GameObject cup = GameObject.Instantiate(CoffeeCupPrefab);
-        cup.transform.position = gameObject.transform.position;
-        this.gameObject.SetActive(false);
-        //Destroy(this);
+        if (active)
+        {
+            time = 1;
+            active = false;
+            pos = gameObject.transform.position;
+        }
+    }
+
+    void Update()
+    {
+        if (!active)
+        {
+            if (time < 0)
+            {
+                //create new cup
+                GetComponent<Collider>().isTrigger = false;
+                GetComponent<Rigidbody>().isKinematic = false;
+                GameObject cup = GameObject.Instantiate(CoffeeCupPrefab);
+                cup.transform.position = pos;
+                time = 100000;
+            }
+            else
+            {
+                time -= InGameTime.deltaTime;
+            }
+        }
     }
 }
