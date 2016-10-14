292ec32fb8b84038963b10bda7b79ed02bb4e091
CabbageOni <pos02232@gmail.com>
2016-10-14 01:20:34 -0700

    added sprite, fixing timer

--- Prototype/Assets/Scripts/NPC/Bianca.cs
+++ Prototype/Assets/Scripts/NPC/Bianca.cs
@@ -3,37 +3,8 @@ using System.Collections;
 
 public class Bianca : MonoBehaviour
 {
-    public RuntimeAnimatorController DeepBreath;
-    public RuntimeAnimatorController ExcitedTalk;
-    public RuntimeAnimatorController HipTalkIdle;
-    Animator anim;
-
-    void Awake ()
-    {
-        anim = transform.root.GetComponent<Animator>();
-    }
-
     void OnMouseUp ()
     {
         MainGameManager.Get.SceneChangeManager.SetCurrentScene(CurrentScene.Dialogue);
-    }
-
-    void Update ()
-    {
-        //Temporary code!!!
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (anim.runtimeAnimatorController == DeepBreath)
-            {
-                anim.runtimeAnimatorController = ExcitedTalk;
-            }
-            else if (anim.runtimeAnimatorController == ExcitedTalk)
-            {
-                anim.runtimeAnimatorController = HipTalkIdle;
-            }
-            else
-                anim.runtimeAnimatorController = DeepBreath;
-            anim.Play(0);
-        }
     }
 }
